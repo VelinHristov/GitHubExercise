64b57e86470c8c95113e99919dc074d5c5b13e00
VelinHristov <resingdora@abv.bg>
2017-05-30 19:22:55 +0300

    firstCommit

---
+++ FirstCommit/FirstCommit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ElementofMaxValue
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int n = int.Parse (Console.ReadLine ());
+			int sumOfAll = 0;
+			int biggest = int.MinValue;
+
+			for (int i = 0; i < n; i++)
+			{
+
+				int inputNumber = int.Parse (Console.ReadLine ());
+				sumOfAll += inputNumber;
+				if (inputNumber > biggest) {
+					biggest = inputNumber;
+				}
+			}
+			int sumOfRest = sumOfAll - biggest;
+			if (sumOfRest == biggest) 
+			{ 
+				Console.WriteLine ("Yes\nSum ={0}", biggest);
+			}
+			else
+			{
+				Console.WriteLine("No\nDiff = {0}", Math.Abs(biggest-sumOfRest));
+			}
+		}
+	}
+}
